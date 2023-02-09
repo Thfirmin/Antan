cee6ff6858a4b8d25f1ece4d8fe14c9870fb11a8
thfirmin <thiagofirmino2001@gmail.com>
2023-02-09 06:59:34 -0300

    Add srcs

---
+++ srcs/antan_main.c
@@ -0,0 +1,17 @@
+#include "antan.h"
+
+int	main(int argc, char *argv[])
+{
+	if (antan_isvalid_arg(argc, argv))
+		antan_error();
+	return (0);
+}
+
+// <number> <number>
+// <flag> <number> <flag> <number>
+// <flag> <base> <number> <flag> <base> <number>
+
+static int	antan_isvalid_arg(int argc, char *argv[])
+{
+	if 
+}
